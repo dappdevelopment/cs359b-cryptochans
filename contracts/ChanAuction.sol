319368d7ca878ccd8afd79fd7ac2b5d4330aead4
Holly Liang <holly@DN51t4q5.SUNet>
2018-04-29 23:38:48 -0700

    Initial commit with truffle files

---
+++ contracts/ChanAuction.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.21;
+
+import './ChanOwnership.sol';
+import './Auction/SaleClockAuction.sol';
+
+contract ChanAuction is ChanOwnership {
+
+    // The address of the ClockAuction contract that handles auctions of Chans. This
+    // same contract handles both peer-to-peer auctions as well as the minted Chan auctions.
+    SaleClockAuction public saleAuction;
+
+    // Sets the reference to the sale auction.
+    // @param _address - Address of sale contract.
+    function setSaleAuctionAddress(address _address) external onlyOwner {
+        SaleClockAuction candidateContract = SaleClockAuction(_address);
+
+        // Verify that a contract is what we expect
+        require(candidateContract.isSaleClockAuction());
+
+        // Set the new contract address
+        saleAuction = candidateContract;
+    }
+
+    // Put a Chan up for auction.
+    // Does some ownership trickery to create auctions in one tx.
+    function createSaleAuction(
+        uint256 _chanId,
+        uint256 _startingPrice,
+        uint256 _endingPrice,
+        uint256 _duration
+    )
+        external
+        whenNotPaused
+        onlyOwnerOf(_chanId)    //Also verifies whether Chan is already on any auction, since this will throw because it will be owned by the auction contract
+    {
+
+        approve(saleAuction, _chanId);
+        // Sale auction throws if inputs are invalid and clears
+        // transfer and sire approval after escrowing the kitty.
+        saleAuction.createAuction(
+            _chanId,
+            _startingPrice,
+            _endingPrice,
+            _duration,
+            msg.sender
+        );
+    }
+
+    // Transfers the balance of the sale auction contract
+    // to the ChanCore contract. We use two-step withdrawal to
+    // prevent two transfer calls in the auction bid function.
+    function withdrawAuctionBalances() external onlyOwner {
+        saleAuction.withdrawBalance();
+    }
+}
