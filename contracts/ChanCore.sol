769a34f9c7e20dbc30498560625eb91a90fbbab0
Holly Liang <holly@DN51t4q5.SUNet>
2018-05-02 00:46:47 -0700

    Fixed owner address of first chan since address 0 is not allowed

--- contracts/ChanCore.sol
+++ contracts/ChanCore.sol
@@ -14,7 +14,7 @@ contract ChanCore is ChanAuction {
         paused = true;
 
         // start with the mythical chan 0 - so we don't have generation-0 parent issues
-        _createChan("AnimeGod", address(0), true);
+        _createChan("AnimeGod", this, true);
     }
 
     // Used to mark the smart contract as upgraded, in case there is a serious
