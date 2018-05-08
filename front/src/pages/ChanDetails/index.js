97908ba7fceb584e8f3c2177dabe4dd6f3aa6389
liubaige <baige.liu@mail.utoronto.ca>
2018-05-07 23:25:21 -0700

    no message

--- front/src/pages/ChanDetails/index.js
+++ front/src/pages/ChanDetails/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Grid from 'material-ui/Grid';
 
-// import AsyncCryptoTree from 'components/AsyncCryptoTree';
+// import AsyncCryptoChan from 'components/AsyncCryptoChan';
 
 export default class ChanDetails extends React.Component {
   render() {
@@ -14,7 +14,7 @@ export default class ChanDetails extends React.Component {
         <h1>My Chan Detail</h1>
         <Grid container spacing={24}>
           <Grid item xs={12} md={6} lg={4}>
-            <AsyncCryptoTree contract={contract} id={selectedId} />
+            <AsyncCryptoChan contract={contract} id={selectedId} />
           </Grid>
           <Grid item xs={12} md={6} lg={8}>
             <div style={{float: 'right'}}>
