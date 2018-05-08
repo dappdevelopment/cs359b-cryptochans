41c4b57825c8e0ee3eed5dd4c748f254b86bc8b8
liubaige <baige.liu@mail.utoronto.ca>
2018-05-08 05:11:16 -0700

    calculate time spent on the web page.

--- front/src/pages/ChanDetails/index.js
+++ front/src/pages/ChanDetails/index.js
@@ -6,8 +6,37 @@ import Grid from 'material-ui/Grid';
 
 export default class ChanDetails extends React.Component {
   render() {
-    const { match, contract } = this.props;
-    const selectedId = match.params.id;
+        let startDate = new Date();
+        let elapsedTime = 0;
+
+        const focus = function() {
+            startDate = new Date();
+        };
+
+        const blur = function() {
+            const endDate = new Date();
+            const spentTime = endDate.getTime() - startDate.getTime();
+            elapsedTime += spentTime;
+            console.log(elapsedTime);
+        };
+
+        const beforeunload = function() {
+            const endDate = new Date();
+            const spentTime = endDate.getTime() - startDate.getTime();
+            elapsedTime += spentTime;
+            console.log(elapsedTime);
+
+            // elapsedTime contains the time spent on page in milliseconds
+        };
+
+        window.addEventListener('focus', focus);
+        window.addEventListener('blur', blur);
+        window.addEventListener('beforeunload', beforeunload);
+
+
+
+        const { match, contract } = this.props;
+        const selectedId = match.params.id;
 
     return (
       <div>
