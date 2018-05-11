ac7023e41bfdc15425d368abf72a141f4634797c
liubaige <baige.liu@mail.utoronto.ca>
2018-05-11 12:17:10 -0700

    add buy chan button

--- src/pages/MarketPlace/index.js
+++ src/pages/MarketPlace/index.js
@@ -18,26 +18,26 @@ export default class BuyNewChan extends React.Component {
 
   // }
 
-    buy(){
+    buy(chan_id){
         console.log("bought");
-
+        console.log(chan_id);
+        this.ChanCoreContract.ownerOf(chan_id).then(result=> {console.log(result,typeof(result)); const owner = result });
     }
 
 
-
-
   render() {
-        const { match, contract, contract2} = this.props;
+        const { match, contract1, contract2} = this.props;
         // const selectedId = match.params.id;
         console.log(contract2);
-        this.cryptoChanContract = contract2;
+        this.ChanCoreContract = contract2;
+        this.SaleAuctionCoreContract = contract1;
 
         // const myChans = this.cryptotreesContract.getMyChans();
         const i1 ="http://img.im17.com/upload/cimg/2012/09-26/CV4VR32635714142861850668.jpg"
         const i2="http://img.wxcha.com/file/201711/28/0ba7b1180e.jpg?down"
         const fake_data =[{"url":i1},{"url":i2}];
         const buy_func = this.buy.bind(this);
-
+        const getChanDetail = this.ChanCoreContract.getChan;
 
 
     return (
@@ -47,7 +47,10 @@ export default class BuyNewChan extends React.Component {
       {fake_data.map(function(d, idx){
         console.log(buy_func);
          return (<li><img height="200" width="200" src={d.url} key={idx}/>
-          <button id="withdraw" onClick={buy_func}>
+         <p>
+         {getChanDetail.bind(null,idx).then(result=> {console.log(result,typeof(result)); return result;})}
+         </p>
+          <button id="withdraw" onClick={buy_func.bind(null,idx)}>
         Buy!
         </button>
 
